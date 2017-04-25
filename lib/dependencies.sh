a10283482a2145181bdf89069c46bcd04526e404
MikaXII <mikael.joubert@supinfo.com>
2017-04-25 11:53:29 +0200

    Skipping rebuild dependencies

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -71,7 +71,7 @@ npm_node_modules() {
     else
       echo "Installing node modules (package.json)"
     fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc --cache $build_dir/.npm 2>&1
+   // npm install --unsafe-perm --userconfig $build_dir/.npmrc --cache $build_dir/.npm 2>&1
   else
     echo "Skipping (no package.json)"
   fi
